2384dcbe83357a9bff76c54a6ceac079ac08ecc6
brlnstyn <berlianasetyani261@gmail.com>
2022-05-08 20:29:41 +0700

    first upload

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,405 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Perkembangan Islam di Nusantara</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+    <link rel="stylesheet" href="assets/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+
+    <link rel="stylesheet" href="assets/css/aos.css">
+
+    <link rel="stylesheet" href="assets/css/ionicons.min.css">
+
+    <link rel="stylesheet" href="assets/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="assets/css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/icomoon.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://kit.fontawesome.com/35272ed214.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    
+	  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+	      <a class="navbar-brand" href="#">CHRONALOGY</a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> 
+	      </button>
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+	        	<li class="nav-item active"><a href="#" class="nav-link">Home</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+    
+    <section class="home-slider js-fullheight owl-carousel bg-white">
+      <div class="slider-item js-fullheight">
+      	<div class="overlay"></div>
+        <div class="container">
+          <div class="row d-md-flex no-gutters slider-text js-fullheight align-items-center justify-content-end" data-scrollax-parent="true">
+          	<div class="one-third order-md-last img js-fullheight" style="background-image:url(assets/images/header2.jpg);">
+          		<h3 class="vr"></h3>
+          	</div>
+	          <div class="one-forth d-flex js-fullheight align-items-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+	          	<div class="text">
+		            <h1 class="mb-4">Masuk dan Berkembangnya <br>Islam di Nusantara</h1>
+		            <p></p>
+	            </div>
+	          </div>
+        	</div>
+        </div>
+      </div>
+    </section>
+		
+		<section class="ftco-section ftc-no-pb">
+			<div class="container">
+				<div class="row no-gutters">
+					<div class="col-md-5 p-md-5 img img-2" style="background-image: url(assets/images/pict_1.jpeg);">
+					</div>
+					<div class="col-md-7 wrap-about pb-md-5 ftco-animate">
+	          <div class="heading-section mb-5 pl-md-5">
+	          	<div class="pl-md-5 ml-md-5">
+		          	<span class="subheading subheading-with-line"><small class="pr-2 bg-white">Pendahuluan</small></span>
+		            <h2 class="mb-4">Sekilas Mengenai Munculnya Agama Islam di Indonesia</h2>
+	            </div>
+	          </div>
+	          <div class="pl-md-5 ml-md-5 mb-5">
+							<p>Agama Islam lahir di Mekkah (Arab Saudi) tahun 610 Masehi yang diwahyukan kepada Nabi Muhammad SAW melalui Malaikat Jibdil. Dalam proses penyebaran agama Islam, Nabi Muhammad SAW sempat melakukan hijrah dari Mekkah ke Madinah.</p>
+							<p>Agama Islam di Nusantara sudah berkemban pada abad ke-13 Masehi yang ditandai dengan adanya kerajaan atau kesultanan islam pertama di Indonesia yaitu Kesultanan Samudra Pasai yang terletak di ujung Pulau Sumatra.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+    <section class="ftco-services">
+			<div class="container">
+				<div class="row justify-content-start mb-5 pb-5">
+          <div class="col-md-4 heading-section ftco-animate">
+            <h2 class="mb-4">Teori-Teori</h2>
+          </div>
+          <div class="col-md-8 pl-md-5 ftco-animate">
+          	<div class="pl-md-4 border-line">
+	          	<p>Ada 3 teori mengenai asal usul kedatangan dan penyebaran islam di Nusantara, berikut disajikan teori-teori tersebut</p>
+          	</div>
+          </div>
+        </div>
+				<div class="row">
+          <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-block">
+              <div class="icon d-flex justify-content-center align-items-center">
+            		<span class="flaticon-idea"></span>
+              </div>
+              <div class="media-body p-2 mt-3">
+                <h3 class="heading">Teori Gujarat</h3>
+                <p>Agama islam masuk ke Indonesia pada abad 13 dan pembawanya berasal dari Gujarat, India. Pendukung teori ini adalah Snouck Hurgronye, WF Stutterheim, dan Bernand H.M. Vlekke.</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-block">
+              <div class="icon d-flex justify-content-center align-items-center">
+            		<span class="flaticon-compass-symbol"></span>
+              </div>
+              <div class="media-body p-2 mt-3">
+                <h3 class="heading">Teori Mekkah</h3>
+                <p>Islam masuk ke Indonesia pada abad ke-7 dan pembawanya berasal dari Arab (Mesir). Pendukung teori ini adalah Hamka, Van Leur, dan T.W. Arnold.</p>
+              </div>
+            </div>    
+          </div>
+          <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-block">
+              <div class="icon d-flex justify-content-center align-items-center">
+            		<span class="flaticon-layers"></span>
+              </div>
+              <div class="media-body p-2 mt-3">
+                <h3 class="heading">Teori Persia</h3>
+                <p>Islam masuk ke Indonesia pada abad 13 dan pembawanya berasal dari Persia (Iran). Dasar teori ini adalah kesamaan budaya Persia dengan budaya masyarakat islam Indonesia.</p>
+              </div>
+            </div>      
+          </div>
+        </div>  
+
+        <section class="ftco-section bg-light">
+			<div class="container">
+				<div class="row justify-content-start mb-5 pb-2">
+          <div class="col-md-4 heading-section ftco-animate">
+          	<span class="subheading subheading-with-line"><small class="pr-2 bg-white">Points</small></span>
+            <h2 class="mb-4">Saluran Penyebaran</h2>
+          </div>
+          <div class="col-md-8 pl-md-5 heading-section ftco-animate">
+          	<div class="pl-md-4 border-line">
+	          	<p>Berikut ini beberapa saluran penyebaran islam di Nusantara:</p>
+          	</div>
+          </div>
+        </div>	
+				<div class="row">
+          <div class="col-md-4 ftco-animate">
+            <div class="blog-entry">
+              <a href="blog-single.html" class="block-20" style="background-image: url('assets/images/pernikahan.jpg');">
+              </a>
+              <div class="text d-flex py-4">
+                <div class="desc pl-3">
+	                <h3 class="heading">Pernikahan</h3>
+	              </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 ftco-animate">
+            <div class="blog-entry">
+              <a href="blog-single.html" class="block-20" style="background-image: url('assets/images/perdagangan.jpg');">
+              </a>
+              <div class="text d-flex py-4">
+                <div class="desc pl-3">
+	                <h3 class="heading">Perdagangan</h3>
+	              </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 ftco-animate">
+            <div class="blog-entry">
+              <a href="blog-single.html" class="block-20" style="background-image: url('assets/images/pendidikan.jpg');">
+              </a>
+              <div class="text d-flex py-4">
+                <div class="desc pl-3">
+	                <h3 class="heading">Pendidikan</h3>
+	              </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 ftco-animate">
+            <div class="blog-entry" data-aos-delay="100">
+              <a href="blog-single.html" class="block-20" style="background-image: url('assets/images/tasawuf.jpeg');">
+              </a>
+              <div class="text d-flex py-4">
+                <div class="desc pl-3">
+	                <h3 class="heading">Ajaran Tasawuf</h3>
+	              </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 ftco-animate">
+            <div class="blog-entry" data-aos-delay="100">
+              <a href="blog-single.html" class="block-20" style="background-image: url('assets/images/kesenian.jpg');">
+              </a>
+              <div class="text d-flex py-4">
+                <div class="desc pl-3">
+	                <h3 class="heading">Kesenian</h3>
+	              </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 ftco-animate">
+            <div class="blog-entry" data-aos-delay="200">
+              <a href="blog-single.html" class="block-20" style="background-image: url('assets/images/dakwah.png');">
+              </a>
+              <div class="text d-flex py-4">
+                <div class="desc pl-3">
+	                <h3 class="heading">Dakwah</h3>
+	              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+			</div>
+		</section>
+
+        <div class="row justify-content-start mt-5 py-5">
+          <div class="col-md-4 heading-section ftco-animate">
+            <h2 class="mb-4">Penyebaran Islam</h2>
+            <p>Penyebaran islam dilakukan secara damai sehingga dapat diterima oleh masyarakat Nusantara. Hal tersebut juga didukung oleh beberapa faktor, diantaranya:</p>
+          </div>
+          <div class="col-md-8 ftco-animate">
+          	<div class="row d-flex">
+          		<div class="col-md-6 d-flex align-items-stretch">
+          			<div class="media block-6 services services-2 d-block bg-light p-4 mb-4">
+		              <div class="icon d-flex justify-content-center align-items-center">
+		            		<span class="flaticon-layers"></span>
+		              </div>
+		              <div class="media-body p-2 mt-3">
+		                <h3 class="heading">Satu</h3>
+		                <p>Cara masuk Islam sangat mudah, yaitu dengan membaca 2 kalimat syahadat.</p>
+		              </div>
+		            </div>
+          		</div>
+          		<div class="col-md-6 d-flex align-items-stretch">
+          			<div class="media block-6 services services-2 d-block bg-light p-4 mb-4">
+		              <div class="icon d-flex justify-content-center align-items-center">
+		            		<span class="flaticon-layers"></span>
+		              </div>
+		              <div class="media-body p-2 mt-3">
+		                <h3 class="heading">Dua</h3>
+		                <p>Tata cara peribadatan yang sangat sederhana serta tidak mengenal lapisan sosial.</p>
+		              </div>
+		            </div>
+          		</div>
+          	</div>
+          </div>
+        </div>
+			</div>
+		</section>
+
+
+    <section class="ftco-section testimony-section bg-light">
+      <div class="container">
+        <div class="row justify-content-center mb-5 pb-3">
+          <div class="col-md-7 heading-section ftco-animate">
+          	<span class="subheading subheading-with-line"><small class="pr-2 bg-light">Kebudayaan</small></span>
+            <h2 class="mb-4">Akulturasi Kebudayaan</h2>
+            <p>Berikut beberapa akulturasi kebudayaan Indonesia dengan islam:</p>
+          </div>
+        </div>
+        <div class="row ftco-animate">
+          <div class="col-md-12">
+            <div class="carousel-testimony owl-carousel">
+              <div class="item">
+                <div class="testimony-wrap p-4 pb-5">
+                  <div class="user-img mb-5" style="background-image: url(assets/images/bangunan.jpg)">
+                    <span class="quote d-flex align-items-center justify-content-center">
+                      <i class="icon-quote-left"></i>
+                    </span>
+                  </div>
+                  <div class="text">
+                    <p class="mb-5 pl-4 line">Susunan atap bertingkat merupakan akulturasi islam dan hindu-buddha.</p>
+                    <div class="pl-5">
+	                    <p class="name">Seni Bangunan</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="item">
+                <div class="testimony-wrap p-4 pb-5">
+                  <div class="user-img mb-5" style="background-image: url(assets/images/rupa.jpg)">
+                    <span class="quote d-flex align-items-center justify-content-center">
+                      <i class="icon-quote-left"></i>
+                    </span>
+                  </div>
+                  <div class="text">
+                    <p class="mb-5 pl-4 line">Memadukan seni lukis dan ukir menggunakan tulisan arab.</p>
+                    <div class="pl-5">
+	                    <p class="name">Seni Rupa</p>
+	                  </div>
+                  </div>
+                </div>
+              </div>
+              <div class="item">
+                <div class="testimony-wrap p-4 pb-5">
+                  <div class="user-img mb-5" style="background-image: url(assets/images/pemerintahan.jpg)">
+                    <span class="quote d-flex align-items-center justify-content-center">
+                      <i class="icon-quote-left"></i>
+                    </span>
+                  </div>
+                  <div class="text">
+                    <p class="mb-5 pl-4 line">Dalam sistem pemerintahannya raja diganti dengan sebutan sultan.</p>
+                    <div class="pl-5">
+	                    <p class="name">Sistem Pemerintahan</p>
+	                  </div>
+                  </div>
+                </div>
+              </div>
+              <div class="item">
+                <div class="testimony-wrap p-4 pb-5">
+                  <div class="user-img mb-5" style="background-image: url(assets/images/penanggalan.jpg)">
+                    <span class="quote d-flex align-items-center justify-content-center">
+                      <i class="icon-quote-left"></i>
+                    </span>
+                  </div>
+                  <div class="text">
+                    <p class="mb-5 pl-4 line">Sistem kalendernya menggunakan penanggalan hijriah.</p>
+                    <div class="pl-5">
+	                    <p class="name">Sistem Kalender</p>
+	                  </div>
+                  </div>
+                </div>
+              </div>
+              <div class="item">
+                <div class="testimony-wrap p-4 pb-5">
+                  <div class="user-img mb-5" style="background-image: url(assets/images/hikayat.jpg)">
+                    <span class="quote d-flex align-items-center justify-content-center">
+                      <i class="icon-quote-left"></i>
+                    </span>
+                  </div>
+                  <div class="text">
+                    <p class="mb-5 pl-4 line">Contohnya seperti Hikayat, Suluk, Primbon, dan Babad.</p>
+                    <div class="pl-5">
+	                    <p class="name">Seni Sastra dan Aksara</p>
+	                  </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+			
+    <footer class="ftco-footer ftco-bg-dark ftco-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">CHRONALOGY</h2>
+              <p>Hi! Terima kasih sudah berkunjung dan membaca tulisan saya :)</p>
+            </div>
+          </div>
+          <div class="col-md">
+             <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Kontak</h2>
+              <ul class="list-unstyled">
+                <li><a href=""><i class="fa-solid fa-envelope"></i></span>  berlianasetyani261@gmail.com</a></li>
+                <li><a href="https://www.instagram.com/brlnstyn_/"><i class="fa-brands fa-instagram"></i></span>  @brlnstyn_</a></li>
+                <li><a href="https://www.linkedin.com/in/berliana-setyani-142139228/"><i class="fa-brands fa-linkedin"></i></span>  Berliana Setyani</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+
+            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="assets/js/popper.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/jquery.easing.1.3.assets/"></script>
+  <script src="assets/js/jquery.waypoints.min.js"></script>
+  <script src="assets/js/jquery.stellar.min.js"></script>
+  <script src="assets/js/owl.carousel.min.js"></script>
+  <script src="assets/js/jquery.magnific-popup.min.js"></script>
+  <script src="assets/js/aos.js"></script>
+  <script src="assets/js/jquery.animateNumber.min.js"></script>
+  <script src="assets/js/bootstrap-datepicker.js"></script>
+  <script src="assets/js/jquery.timepicker.min.js"></script>
+  <script src="assets/js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="assets/js/google-map.js"></script>
+  <script src="assets/js/main.js"></script>
+    
+  </body>
+</html>
